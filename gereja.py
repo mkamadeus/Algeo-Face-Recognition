e3fdc28e937b15479d09fe8960475519ce9871d5
stanleyyoga123 <yoga360227@gmail.com>
2019-10-23 23:29:54 +0700

    added cos similarity

--- gereja.py
+++ gereja.py
@@ -65,6 +65,29 @@ class Matcher(object):
         self.matrix = np.array(self.matrix)
         self.names = np.array(self.names)
 
+    def cos_cdist(self, vector):
+        # getting cosine distance between search image and images database
+        v = vector.reshape(len(vector),1)
+        lengthv = 0
+        print(len(v))
+        for i in range(2048):
+            lengthv += v[i]**2
+        lengthv = math.sqrt(lengthv)
+        a = []
+        for i in range(len(self.matrix)):
+            b = 0
+            w = self.matrix[i].reshape(len(vector),1)
+            lengthw = 0
+            for j in range(2048):
+                lengthw += w[j]**2
+                b += v[j]*w[j]
+            lengthw = math.sqrt(lengthw)
+            c = b/(lengthv*lengthw)
+            a.append(1-c)
+        a = np.array(a).reshape(-1)
+        print(a)
+        return a
+
     def euclidean_distance(self, vector):
         # getting euclidean distance between search image and images database
 
@@ -98,6 +121,15 @@ class Matcher(object):
         # nearest_img_paths = self.names[nearest_ids].tolist()
 
         return nearest_img_paths, img_distances
+    
+    def match_cost(self, image_path, topn=5):
+        features = extract_features(image_path)
+        img_distances = self.cos_cdist(features)
+        # getting top 5 records
+        nearest_ids = np.argsort(img_distances)[:topn].tolist()
+        nearest_img_paths = self.names[nearest_ids].tolist()
+
+        return nearest_img_paths, img_distances[nearest_ids].tolist()
 
 def show_img(path):
     img = cv2.imread(path, 1)
