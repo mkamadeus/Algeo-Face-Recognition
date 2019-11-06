27f0112874f9086b6263e796a983e912476cf028
stanleyyoga123 <yoga360227@gmail.com>
2019-11-06 18:46:48 +0700

    add comment and fixed next prev

--- gui_2.py
+++ gui_2.py
@@ -21,6 +21,7 @@ class faceRecognition(tk.Tk):
         self.switch_frame(StartPage)
 
     def switch_frame(self, frame_class):
+        #Destroy frame and show new frame
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
@@ -32,43 +33,47 @@ class StartPage(tk.Frame):
     img = None
 
     def showImage(self,filename):
+        #Show image from directory
         im = Image.open(filename)
         im = im.resize((300,300), Image.ANTIALIAS)
         tkimage = ImageTk.PhotoImage(im)
         self.img = Button(self, image=tkimage, command=filename)
         self.img.image=tkimage 
-        self.img.grid(row=3,column=3)
+        self.img.grid(row=2,column=1)
 
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        master.geometry("1000x1000")
+        master.geometry("500x500")
         label = tk.Label(self, text="Choose a method: ")
         label.grid(row=0, column=1)
 
         r = tk.IntVar()
-        rbutton1 = tk.Radiobutton(self, text="Euclidean Distance", variable=r, value=1, width=20).grid(row=2, column=1)
-        rbutton2 = tk.Radiobutton(self, text="Cosine Similarity", variable=r, value=2, width=20).grid(row=3, column=1)
-        button1 = tk.Button(self, text="OK", command=lambda: master.switch_frame(cosMethod) if r==1 else master.switch_frame(eucliMethod), height=1, width=10).grid(row=2, column=3)
-        checkbox = tk.Checkbutton(self, text="Randomize Input", width=24).grid(row=4,column=3)
+        rbutton1 = tk.Radiobutton(self, text="Euclidean Distance", variable=r, value=1, width=20).grid(row=3, column=1)
+        rbutton2 = tk.Radiobutton(self, text="Cosine Similarity", variable=r, value=2, width=20).grid(row=4, column=1)
+        button1 = tk.Button(self, text="OK", command=lambda: master.switch_frame(cosMethod) if r==1 else master.switch_frame(eucliMethod), height=1, width=10).grid(row=5, column=1)
+        checkbox = tk.Checkbutton(self, text="Randomize Input", width=24).grid(row=6,column=1)
 
-        self.labelFrame = ttk.LabelFrame(self, text = "Open File")
-        self.labelFrame.grid(column = 0, row = 1, padx = 20, pady = 20)
+        self.labelFrame = ttk.LabelFrame(self)
         self.button()
         
         self.grid_columnconfigure(0, minsize=16)
 
     def button(self):
+        #Browse file button
         self.button = ttk.Button(self, text= "Browse A File", command= self.fileDialog)
         self.button.grid(column = 1, row = 1)
     
     def fileDialog(self):
+        #Command after button clicked
         self.filename = filedialog.askopenfilename(initialdir =  "/", title = "Select A File", filetype = (("jpeg files","*.jpg"),("all files","*.*")) )
         self.label = ttk.Label(self.labelFrame, text = "")
         self.label.grid(column = 1, row = 2)
-        self.label.configure(text = self.filename)
-        print(self.filename)
+
+        #When image is shown before, image will be destroyed 
         if self.img is not None:
             self.img.destroy()
+        
+        #When filename is not None, it will call showImage
         if self.filename is not None:
             self.showImage(self.filename)
 
@@ -81,37 +86,45 @@ class eucliMethod(tk.Frame):
     img = None
 
     def nexts(self):
-        self.pic+=1
+        self.pic= (self.pic+1) % 5
         ma = fm.Matcher('features.pck')
         names_cosine, match_cosine = ma.match_cosine_similarity(self.sample[0], topn=5)
         img_arr = mpimg.imread(os.path.join(self.images_path, names_cosine[self.pic]))
         im = Image.open(os.path.join(self.images_path, names_cosine[self.pic]))
         
+        #It will destroy image
         if self.img is not None:
             self.img.destroy()
 
+        #Show image
         tkimage = ImageTk.PhotoImage(im)
         self.img = Button(self, image=tkimage, command=os.path.join(self.images_path, names_cosine[self.pic]))
         self.img.image=tkimage
         self.img.pack()
 
+        print(self.pic)
+
     def prev(self):
-        self.pic-=1
+        self.pic= (self.pic-1) % 5
         ma = fm.Matcher('features.pck')
         names_cosine, match_cosine = ma.match_cosine_similarity(self.sample[0], topn=5)
         img_arr = mpimg.imread(os.path.join(self.images_path, names_cosine[self.pic]))
         im = Image.open(os.path.join(self.images_path, names_cosine[self.pic]))
 
+        #It will destroy image
         if self.img is not None:
             self.img.destroy()
 
+        #Show Image
         tkimage = ImageTk.PhotoImage(im)
         self.img = Button(self, image=tkimage, command=os.path.join(self.images_path, names_cosine[self.pic]))
         self.img.image=tkimage
         self.img.pack()
 
+        print(self.pic)
+
     def __init__(self, master):
-        master.geometry("600x600")
+        master.geometry("500x500")
         tk.Frame.__init__(self, master)
 
         label = tk.Label(self, text="Euclidean Distance")
@@ -126,14 +139,9 @@ class eucliMethod(tk.Frame):
         button3 = tk.Button(self, text="Back to The Start Page", command=lambda: master.switch_frame(StartPage))
         button3.pack()
 
-    def show_img(path):
-        img = cv2.imread(path, 1)
-        plt.imshow(img)
-        plt.show()
-
 class cosMethod(tk.Frame):
     def __init__(self, master):
-        master.geometry("600x600")
+        master.geometry("500x500")
         tk.Frame.__init__(self, master)
         label = tk.Label(self, text="Cosine Similarity")
         label.grid(row=1,column=5)
